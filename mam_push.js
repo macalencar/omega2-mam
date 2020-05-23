aa1052159456208b20a1ba62b37c88fa9d3ee9df
Márcio Alencar <macalencar@gmail.com>
2020-05-23 06:27:11 -0400

    Update mam_push.js

--- mam_push.js
+++ mam_push.js
@@ -3,13 +3,12 @@ const Mam = require('@iota/mam')
 const { asciiToTrytes, trytesToAscii } = require('@iota/converter')
 const mode = 'public'
 const provider = 'https://nodes.devnet.iota.org'
-
-const mamExplorerLink = `https://mam-explorer.firebaseapp.com/?provider=${encodeURIComponent(provider)}&mode=${mode}&root=`
-
+//receive parameter(json object)
 const readingValues = process.argv[2]
 
-let mamState = Mam.init(provider)
 // Initialise MAM State
+let mamState = Mam.init(provider)
+// Recover last MAM State
 try{
         let lastState = JSON.parse(fs.readFileSync('lastT.json'))
         mamState["channel"]["next_root"]=lastState["channel"]["next_root"]
@@ -23,16 +22,17 @@ const publish = async packet => {
     const message = Mam.create(mamState, trytes)
     mamState = message.state
     await Mam.attach(message.payload, message.address, 3, 9)
-    //console.log('Published', packet, '\n');
-    //console.log("Interno:",message.root)
+	//SAVE ROOT NODE
     if (!fs.existsSync('firstT.id')){
             fs.writeFile('firstT.id',message.root,function(err){})
     }
+	//UPDATE LAST STATE
     fs.writeFile('lastT.json',JSON.stringify(mamState), function(err){});
     return message.root
 }
-
 jPacket = JSON.parse(readingValues)
 jPacket["timestamp"]=(new Date()).toLocaleString()
-
 publish(jPacket)
+//Check your stream at:
+// https://mam-explorer.firebaseapp.com/?provider=https%3A%2F%2Fnodes.devnet.iota.org&mode=public&root=<ROOT ID>
+//<ROOT_ID>: Check in ‘firstT.id’ file 
